30725b2ead70ee756c3ce714f5d2312a93adb18b
Brett Ernst <brett.ernst@iconmobile.com>
2014-09-22 22:28:13 -0700

    add Timeline class to manage complex animations

---
+++ src/shared/Math/Timeline/KeyChannel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using GameStack;
+using GameStack.Graphics;
+
+namespace GameStack
+{
+	public interface IKeyChannel {
+		void Update (double t);
+	}
+
+	public class KeyChannel<T> : IKeyChannel {
+		Action<T> _setter;
+		SortedList<double, KeyFrame<T>> _keyframes;
+		T _value;
+
+		public KeyChannel (Action<T> setter, params KeyFrame<T>[] keyframes) {
+			_keyframes = new SortedList<double, KeyFrame<T>>();
+			_setter = setter;
+			if (keyframes != null) {
+				this.AddRange(keyframes);
+			}
+		}
+
+		public T Value { get { return _value; } }
+
+		public void Add(KeyFrame<T> keyframe) {
+			_keyframes[keyframe.Time] = keyframe;
+		}
+
+		public void Add(params KeyFrame<T>[] keyframes) {
+			this.AddRange(keyframes);
+		}
+
+		public void AddRange(IEnumerable<KeyFrame<T>> keyframes) {
+			foreach (var k in keyframes)
+				this.Add(k);
+		}
+
+		void IKeyChannel.Update (double t) {
+			if (_keyframes.Count == 0)
+				return;
+
+			KeyFrame<T> k1, k2;
+			var rt = this.GetKeysFromTime(t, out k1, out k2);
+			_value = k1.Value;
+			if (k2 != null && k2.Function != null)
+				_value = k2.Function(k1.Value, k2.Value, (float)rt);
+			if (_setter != null)
+				_setter(_value);
+		}
+
+		double GetKeysFromTime(double t, out KeyFrame<T> k1, out KeyFrame<T> k2) {
+			k1 = _keyframes.Values[0];
+			k2 = null;
+			int i;
+			for (i = 0; i < _keyframes.Keys.Count; i++) {
+				if (t < _keyframes.Keys[i])
+					break;
+				k1 = _keyframes.Values[i];
+			}
+			k2 = (i == 0 || i >= _keyframes.Count) ? null : _keyframes.Values[i];
+			if(k2 == null)
+				return 0;
+			else
+				return (t - k1.Time) / (k2.Time - k1.Time);
+		}
+	}
+
+	public class KeyFrame<T> {
+		public readonly double Time;
+		public readonly T Value;
+		public readonly TweenFunc<T> Function;
+
+		public KeyFrame(double time, T value, TweenFunc<T> func) {
+			Time = time;
+			Value = value;
+			Function = func;
+		}
+	}
+}
+
---
+++ src/shared/Math/Timeline/Timeline.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using GameStack;
+using GameStack.Graphics;
+
+namespace GameStack {
+	public class Timeline : IUpdater {
+		Dictionary<string,IKeyChannel> _channels;
+		double _duration, _time, _rate;
+		bool _isPlaying;
+
+		public Timeline (double duration) {
+			_duration = duration;
+			_channels = new Dictionary<string, IKeyChannel>();
+		}
+
+		public double Duration { get { return _duration; } set { _duration = value; } }
+
+		public bool IsPlaying { get { return _isPlaying; } set { _isPlaying = value; } }
+
+		public double Rate { get { return _rate; } set { _rate = value; } }
+
+		public double Time { get { return _time; } set { _time = value; } }
+
+		public void AddChannel(string name, IKeyChannel channel) {
+			if (_channels.ContainsKey(name))
+				throw new ArgumentException("A channel with the name `" + name + "' already exists.", "name");
+			_channels.Add(name, channel);
+		}
+
+		public bool RemoveChannel(string name) {
+			return _channels.Remove(name);
+		}
+
+		public void Seek (double time) {
+			_time = time;
+			foreach (var chan in _channels.Values) {
+				chan.Update(time);
+			}
+		}
+
+		public void Update(float dt) {
+			if (_isPlaying) {
+				this.Seek(Math.Min(_duration, _time + dt));
+				if (_time >= _duration)
+					_isPlaying = false;
+			}
+		}
+
+		public T Get<T>(string name) {
+			IKeyChannel ichan;
+			if (_channels.TryGetValue(name, out ichan)) {
+				var chan = ichan as KeyChannel<T>;
+				if (chan != null)
+					return chan.Value;
+				else
+					throw new ArgumentException("Incorrect type for channel `" + name + "'.");
+			} else
+				throw new ArgumentException("No such channel named `" + name + "'.");
+		}
+
+		void IUpdater.Update (FrameArgs e) {
+			this.Update(e.DeltaTime);
+		}
+	}
+}
+
