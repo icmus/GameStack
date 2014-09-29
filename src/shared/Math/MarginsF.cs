8269e0ab68296dd99619eac066d6387df5609cc6
Brett Ernst <brett.ernst@iconmobile.com>
2014-09-29 14:28:30 -0700

    rename fields of MarginF

--- src/shared/Math/MarginsF.cs
+++ src/shared/Math/MarginsF.cs
@@ -6,27 +6,17 @@ namespace GameStack
 	[StructLayout(LayoutKind.Sequential)]
 	public struct MarginsF
 	{
-		public float X;
-		public float Y;
-		public float X2;
-		public float Y2;
+		public float Left;
+		public float Top;
+		public float Right;
+		public float Bottom;
 
 		public MarginsF(float left, float top, float right, float bottom) {
-			this.X = left;
-			this.Y = top;
-			this.X2 = right;
-			this.Y2 = bottom;
+			this.Left = left;
+			this.Top = top;
+			this.Right = right;
+			this.Bottom = bottom;
 		}
-
-		public bool IsEmpty { get { return this.X == 0f && this.Y == 0f && this.X2 == 0f && this.Y2 == 0f; } }
-
-		public float Left { get { return X; } set { X = value; } }
-
-		public float Top { get { return Y; } set { Y = value; } }
-
-		public float Right { get { return X2; } set { X2 = value; } }
-
-		public float Bottom { get { return Y2; } set { Y2 = value; } }
 
 		public override bool Equals (object obj)
 		{
@@ -35,11 +25,11 @@ namespace GameStack
 
 		public override int GetHashCode ()
 		{
-			return X.GetHashCode() ^ Y.GetHashCode() ^ X2.GetHashCode() ^ Y2.GetHashCode();
+			return Left.GetHashCode() ^ Top.GetHashCode() ^ Right.GetHashCode() ^ Bottom.GetHashCode();
 		}
 
 		public static bool operator== (MarginsF r1, MarginsF r2) {
-			return r1.X == r2.X && r1.Y == r2.Y && r1.X2 == r2.X2 && r1.Y2 == r2.Y2;
+			return r1.Left == r2.Left && r1.Top == r2.Top && r1.Right == r2.Right && r1.Bottom == r2.Bottom;
 		}
 
 		public static bool operator!= (MarginsF r1, MarginsF r2) {
