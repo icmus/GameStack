27096f37a91738a56a98355f8bc68473f2de3c5f
Brett Ernst <brett.ernst@iconmobile.com>
2015-02-01 02:43:04 -0800

    replace lodepng with pnglite for better performance

--- src/shared/Content/PngLoader.cs
+++ src/shared/Content/PngLoader.cs
@@ -13,6 +13,10 @@ using PixelFormat = OpenTK.Graphics.ES20.All;
 
 namespace GameStack.Content {
 	public static class PngLoader {
+		static PngLoader() {
+			LibPngLite.png_init(IntPtr.Zero, IntPtr.Zero);
+		}
+
 		public static byte[] Decode (Stream stream, out Size size, out PixelFormat pxFormat) {
 			var buf = new byte[stream.Length];
 			stream.Read(buf, 0, buf.Length);
@@ -20,20 +24,54 @@ namespace GameStack.Content {
 			return Decode(buf, out size, out pxFormat);
 		}
 
-		public static byte[] Decode (byte[] pngData, out Size size, out PixelFormat pxFormat) {
-			IntPtr p;
-			int w, h;
-			var error = LibLodePng.Decode32(out p, out w, out h, pngData, (IntPtr)pngData.Length);
-			if (error != 0) {
-				throw new ContentException("Failed to load png");
-				//throw new ContentException ("Error decoding PNG: " + LibLodePng.ErrorText (error));
+		unsafe public static byte[] Decode (byte[] pngData, out Size size, out PixelFormat pxFormat) {
+			fixed (byte* p = pngData) {
+				var cursor = new LibPngLite.PngDataCursor(p, pngData.Length);
+				var png = new LibPngLite.Png();
+
+				var result = LibPngLite.png_open_read(ref png, ReadCallback, ref cursor);
+				if (result < 0)
+					throw new PngException(result);
+				switch (png.bpp) {
+					case 3:
+						pxFormat = PixelFormat.Rgb;
+						break;
+					case 4:
+						pxFormat = PixelFormat.Rgba;
+						break;
+					default:
+						throw new PngException("Unsupported bpp: " + png.bpp);
+				}
+				size = new Size(png.width, png.height);
+				var buf = new byte[png.width * png.height * png.bpp];
+
+				result = LibPngLite.png_get_data(ref png, buf);
+				if (result < 0)
+					throw new PngException(result);
+				return buf;
 			}
-			var buf = new byte[w * h * 4];
-			Marshal.Copy(p, buf, 0, buf.Length);
-			LibLodePng.Free(p);
-			size = new Size(w, h);
-			pxFormat = PixelFormat.Rgba;
-			return buf;
+		}
+
+		[DllImport("libc")]
+		unsafe static extern IntPtr memcpy (byte* dst, byte* src, int len);
+
+		unsafe static int ReadCallback (IntPtr output, int size, int count, ref LibPngLite.PngDataCursor cursor) {
+			var sz = size * count;
+			if (sz > cursor.Length - cursor.Offset)
+				return 0;
+			if (output != IntPtr.Zero) {
+				var dst = (byte*)output;
+				var src = cursor.Data + cursor.Offset;
+				if (sz > 32) {
+					memcpy(dst, src, sz);
+				} else {
+					for (var i = 0; i < sz; i++) {
+						*(dst++) = *(src++);
+					}
+				}
+			}
+			cursor.Offset += sz;
+			return sz;
 		}
 
 		public static byte[] Encode (Stream stream, Size size) {
@@ -43,17 +81,50 @@ namespace GameStack.Content {
 			return Encode(buf, size);
 		}
 
-		public static byte[] Encode (byte[] imgData, Size size) {
-			IntPtr p;
-			IntPtr ps;
-			var error = LibLodePng.Encode32(out p, out ps, imgData, size.Width, size.Height);
-			if (error != 0)
-				throw new ContentException("Failed to save png");
+		unsafe public static byte[] Encode (byte[] imgData, Size size) {
+			var png = new LibPngLite.Png();
 
-			var buf = new byte[(long)ps];
-			Marshal.Copy(p, buf, 0, buf.Length);
-			LibLodePng.Free(p);
-			return buf;
+			var data = new byte[size.Width * size.Height * 4];
+			fixed(byte* p = data) {
+				var cursor = new LibPngLite.PngDataCursor(p, data.Length);
+
+				var result = LibPngLite.png_open_write(ref png, WriteCallback, ref cursor);
+				if (result < 0)
+					throw new PngException(result);
+
+				result = LibPngLite.png_set_data(ref png, size.Width, size.Height, 4, 6, imgData);
+				if (result < 0)
+					throw new PngException(result);
+
+				var output = new byte[cursor.Offset];
+				Marshal.Copy((IntPtr)cursor.Data, output, 0, output.Length);
+				return output;
+			}
+		}
+
+		unsafe static int WriteCallback (IntPtr input, int size, int count, ref LibPngLite.PngDataCursor cursor) {
+			var sz = size * count;
+			if (sz > cursor.Length - cursor.Offset)
+				return 0;
+			var dst = (byte*)cursor.Data + cursor.Offset;
+			var src = (byte*)input;
+			if (sz > 32) {
+				memcpy(dst, src, sz);
+			} else {
+				for (var i = 0; i < sz; i++) {
+					*(dst++) = *(src++);
+				}
+			}
+			cursor.Offset += sz;
+			return sz;
+		}
+	}
+
+	public class PngException : ContentException {
+		public PngException (int code) : base(string.Format("Error {0}: {1}", code, Marshal.PtrToStringAnsi(LibPngLite.png_error_string(code)))) {
+		}
+
+		public PngException (string message) : base(message) {
 		}
 	}
 }
